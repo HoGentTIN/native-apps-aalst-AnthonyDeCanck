d5481189a1f524cb8e426dd290f8de1cc3f6e0cb
Anthony De Canck <antdecan@gmail.com>
2019-11-28 13:49:29 +0100

    Toevoegen CLIENT ID en SECRET

--- app/src/main/java/com/example/androidproject/domain/DiscordMessageDataService.kt
+++ app/src/main/java/com/example/androidproject/domain/DiscordMessageDataService.kt
@@ -1,4 +1,8 @@
 package com.example.androidproject.domain
 
 class DiscordMessageDataService {
+    private val CLIENT_ID = "649577462245883904";
+    private val CLIENT_SECRET = "rj4tCOT8ThCb7nImpTRlycsxFKIpoT1L";
+
+    
 }
