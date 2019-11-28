60d40f7d6736982163040c5074c57a0187cb46b0
Anthony De Canck <antdecan@gmail.com>
2019-11-28 14:05:05 +0100

    Toevoegen retrofit

--- app/src/main/java/com/example/androidproject/domain/DiscordMessageDataService.kt
+++ app/src/main/java/com/example/androidproject/domain/DiscordMessageDataService.kt
@@ -1,8 +1,16 @@
 package com.example.androidproject.domain
 
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
 class DiscordMessageDataService {
     private val CLIENT_ID = "649577462245883904";
     private val CLIENT_SECRET = "rj4tCOT8ThCb7nImpTRlycsxFKIpoT1L";
 
-    
+    private val BASE_URL = "http://localhost:50451/api/discord/callback"
+
+    private val retrofit = Retrofit.Builder()
+        .addConverterFactory(ScalarsConverterFactory.create())
+        .baseUrl(BASE_URL)
+        .build()
 }
